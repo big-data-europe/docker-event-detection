0f427892f5d241c6ebb2fa433e503335cf92bd60
pittarasnikif <pittarasnikif@iit.demokritos.gr>
2016-09-19 17:09:42 +0300

    Update connections settings

--- exec/connections_config.sh
+++ exec/connections_config.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-# Script to set cassandra connection host, port and twitter credentials
+# Script to set cassandra/mysql connection host, port and twitter credentials
 # to the relevant BDE Event detection pilot configuration files.
-# expected arguments: path to an input file
 
-# the input file should contain two lines, as below:
-# twitterConsumerKey twitterConsumerKeySecret twitterAccessTokken twitterAccessTokkenSecret
-# host port
 
 function usage {
 	echo
@@ -26,83 +22,124 @@ function useDefaults {
 	echo "cassandraPort=9042"
 }
 echo
-echo ">Setting cassandra connection parameters and twitter credentials."
+echo ">Setting cassandra/mysql connection parameters and twitter credentials."
 
-# defaults. Twitter default credentials will obviously not work.
-twitterConsumerKey="defaultConsumerKey"
-twitterConsumerKeySecret="defaultConsumerKeySecret"
-twitterAccessTokken="defaultAccessToken"
-twitterAccessTokkenSecret="defaultAccessTokenSecret"
-connectionIP="127.0.0.1"
-connectionPort="9042"
 
-# errors below should happen only when calling this script out of context
-if [ -z $BDE_ROOT_DIR ]; then
-	>&2 echo "Warning: Unset BDE root folder variable."
-	exit 1
-fi
-if [ -z $1 ]; then
-	>&2 echo "Warning: Unset argument variable to $0"
-	useDefaults
 
-elif  [ ! -f $1 ]; then
-	>&2 echo "Warning: The configuration file $1 does not exist."
-	useDefaults
-	
-else
-	# read the file
-	echo "Using configuration file $1"
-	# 1st line for twitter creds
-	twitterInfo="$(sed '1q;d' $1)"  
-	# check
-	if [ $(echo $twitterInfo | wc -w ) -ne 4 ]; then
-		echo "asd"
-		>&2 echo "***Error : Malformed file.Need 4 items for twitter credentials. Check the readme file."
-		usage
-		exit 1
-	fi
-	# set
-	twitterConsumerKey="$(echo $twitterInfo | awk '{print $1}')"
-	twitterConsumerKeySecret="$(echo $twitterInfo | awk '{print $2}')"
-	twitterAccessTokken="$(echo $twitterInfo | awk '{print $3}')"
-	twitterAccessTokkenSecret="$(echo $twitterInfo | awk '{print $4}')"
-	# 2nd line for cassandra host/port
-	connectionInfo="$(sed '2q;d' $1)" 
-	# check
-	if [ $(echo $connectionInfo | wc -w ) -ne 2 ]; then
-		>&2 echo "Need 2 items for connectivity options. Check the readme file."
-		usage
-		exit 1
-	fi
-	# set
-	connectionIP="$(echo $connectionInfo | awk '{print $1}')"
-	connectionPort="$(echo $connectionInfo | awk '{print $2}')"
-fi
+# property files to modify
+paths="$BDE_ROOT_DIR/BDECLustering/res/clustering.properties"
+paths+=" $BDE_ROOT_DIR/BDETwitterListener/res/twitter.properties"
+paths+=" $BDE_ROOT_DIR/BDELocationExtraction/res/location_extraction.properties"
+paths+=" $BDE_ROOT_DIR/BDERSSCrawler/res/newscrawler_configuration.properties"
 
-# files to modify
-paths="$BDE_ROOT_DIR/BDEEventDetection/BDECLustering/res/clustering.properties"
-paths+=" $BDE_ROOT_DIR/BDEEventDetection/BDETwitterListener/res/twitter.properties"
-paths+=" $BDE_ROOT_DIR/BDEEventDetection/BDELocationExtraction/res/location_extraction.properties"
-paths+=" $BDE_ROOT_DIR/BDEEventDetection/BDERSSCrawler/res/newscrawler_configuration.properties"
 # newline-delimit, let's not change IFS
 paths="$(echo $paths | sed  's/ /\n/g' )"
 
-# sed the files with cassandra host/port
-echo "Setting cassandra host:[$connectionIP] , port:[$connectionPort] to"
-for f in $paths ; do
-	echo -e "\t[$f]"
-	sed -i "s/cassandra_hosts.*/cassandra_hosts=$connectionIP/g" $f
-	sed -i "s/cassandra_port.*/cassandra_port=$connectionPort/g" $f
-done
 
-# set the twitter credentials
-echo "Setting twitter credentials:"
-filepath="$BDE_ROOT_DIR/BDEEventDetection/BDETwitterListener/res/twitter.properties";
-printf "\t[%s]\n\t[%s]\n\t[%s]\n\t[%s] \nto\n\t[%s]\n\n" "$twitterConsumerKey" "$twitterConsumerKeySecret" \
-		"$twitterAccessTokken" "$twitterAccessTokkenSecret"  "$filepath"
-sed -i "s/twitterConsumerKey=.*/twitterConsumerKey=$twitterConsumerKey/g" $filepath
-sed -i "s/twitterConsumerKeySecret=.*/twitterConsumerKeySecret=$twitterConsumerKeySecret/g" $filepath
-sed -i "s/twitterAccessTokken=.*/twitterAccessTokken=$twitterAccessTokken/g" $filepath
-sed -i "s/twitterAccessTokkenSecret=.*/twitterAccessTokkenSecret=$twitterAccessTokkenSecret/g"  $filepath
+echo "Setting repository connection parameters and twitter credentials."
+
+## TWITTER
+# format expected:
+# twitterConsumerKey
+# twitterConsumerKeySecret
+# twitterAccessTokken
+# twitterAccessTokkenSecret
+twitterfile="$CONNECTIONS_CONFIG_FOLDER/twitter.conf"
+if [ -f "$twitterfile" ]; then
+
+	twitterConsumerKey="$(cat $twitterfile | tail -1 | head -1)"
+	twitterConsumerKeySecret="$(cat $twitterfile | head -2 | tail -1)"
+	twitterAccessTokken="$(cat $twitterfile | head -3 | tail -1)"
+	twitterAccessTokkenSecret="$(cat $twitterfile | head -4 | tail -1)"
+
+
+	printf "Setting twitter :\n"
+	printf "\tkey: [%s]\n" $twitterConsumerKey
+	printf "\tkeysecret: [%s]\n" $twitterConsumerKeySecret
+	printf "\tacctoken: [%s]\n" $twitterAccessTokken
+	printf "\tacctokensecret: [%s]\n" $twitterAccessTokkenSecret
+
+	# set the twitter credentials
+	sed -i "s/twitterConsumerKey=.*/twitterConsumerKey=$twitterConsumerKey/g" "$BDE_ROOT_DIR/BDETwitterListener/res/twitter.properties"
+	sed -i "s/twitterConsumerKeySecret=.*/twitterConsumerKeySecret=$twitterConsumerKeySecret/g" "$BDE_ROOT_DIR/BDETwitterListener/res/twitter.properties"
+	sed -i "s/twitterAccessTokken=.*/twitterAccessTokken=$twitterAccessTokken/g" "$BDE_ROOT_DIR/BDETwitterListener/res/twitter.properties"
+	sed -i "s/twitterAccessTokkenSecret=.*/twitterAccessTokkenSecret=$twitterAccessTokkenSecret/g" "$BDE_ROOT_DIR/BDETwitterListener/res/twitter.properties"
+
+else
+	echo "(!) No twitter credentials were found at [$twitterfile]!"
+fi
+
+
+
+
+# CASSANDRA
+# expecting format:
+# cassandra_hosts
+# cassandra_port
+# cassandra_keyspace
+# cassandra_cluster_name
+
+cassandrafile="$CONNECTIONS_CONFIG_FOLDER/cassandra.conf"
+
+if [  -f "$cassandrafile" ] ; then
+
+	cassandraHost="$(cat $cassandrafile | head -1 | tail -1)"
+	cassandraPort="$(cat $cassandrafile | head -2 | tail -1)"
+	cassandraKeyspace="$(cat $cassandrafile | head -3 | tail -1)"
+	cassandraCluster="$(cat $cassandrafile | head -4 | tail -1)"
+
+	printf "\nSetting cassandra:\n"
+	printf "\thost: [%s]\n" $cassandraHost
+	printf "\tport: [%s]\n" $cassandraPort
+	printf "\tkeyspace: [%s]\n" $cassandraKeyspace
+	printf "\tclustname: [%s]\n" $cassandraCluster
+	printf "\n\tto files:\n"
+
+	for f in $paths ; do
+		printf "\t%s\n" "$f"
+		sed -i "s/cassandra_hosts.*/cassandra_hosts=$cassandraHost/g" $f
+		sed -i "s/cassandra_port.*/cassandra_port=$cassandraPort/g" $f
+		sed -i "s/cassandra_keyspace.*/cassandra_keyspace=$cassandraKeyspace/g" $f
+		sed -i "s/cassandra_cluster_name.*/cassandra_cluster_name=$cassandraCluster/g" $f
+	done
+
+else
+	echo "(!) No cassandra settings were found at [$cassandrafile]"
+fi
+
+# MYSQL
+# expecting format:
+# databaseHost
+# databaseName
+# databaseUsername
+# databasePassword
+mysqlfile="$CONNECTIONS_CONFIG_FOLDER/mysql.conf"
+if [ -f "$mysqlfile" ]; then
+
+	mysqlinfo="$(cat $mysqlfile | grep -v '#')"
+	databaseHost="$(cat $mysqlfile | head -1 | tail -1)"
+	databaseName="$(cat $mysqlfile | head -2 | tail -1)"
+	databaseUsername="$(cat $mysqlfile | head -3 | tail -1)"
+	databasePassword="$(cat $mysqlfile | head -4 | tail -1)"
+
+	printf "Setting mysql:\n"
+	printf "\thost: [%s]\n" $databaseHost
+	printf "\tdbname: [%s]\n" $databaseName
+	printf "\tusername: [%s]\n" $databaseUsername
+	printf "\tpass: [%s]\n" $databasePassword
+	printf "\n\tto files:\n"
+	for f in $paths ; do
+
+		printf "\t%s\n" "$f"
+		sed -i "s<databaseHost.*<databaseHost=$databaseHost<g" $f
+		sed -i "s<databaseName.*<databaseName=$databaseName<g" $f
+		sed -i "s<databaseUsername.*<databaseUsername=$databaseUsername<g" $f
+		sed -i "s<databasePassword.*<databasePassword=$databasePassword<g" $f
+	done
+
+	else
+		echo "(!) No cassandra settings were found at [$mysqlfile]"
+fi
+
 
 echo "-Done setting connection information."; echo
